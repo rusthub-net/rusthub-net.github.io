f9dddaa33fb7cf191df939dde4b234b9a2a18787
Emmanuel Keller <ekeller@open-search-server.com>
2022-06-21 18:49:44 +0100

    Updates

---
+++ precache-manifest.baf8bedc0fec57d200d144f41cd6fb85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4d990590e651134321",
+    "url": "/static/css/main.5a803f18.chunk.css"
+  },
+  {
+    "revision": "fa4d990590e651134321",
+    "url": "/static/js/main.fa4d9905.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e3205c6af13c620bf99cd340315ba3e3",
+    "url": "/index.html"
+  }
+];
